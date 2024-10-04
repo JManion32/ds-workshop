37b0dcea4a51f3c8ba5ecbdb03a91afde0ebbc5a
Cam LC <124944268+H2re@users.noreply.github.com>
2024-10-04 14:54:25 -0400

    Doubled and reversed
    See how this is.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 int main(int argc, char* argv[]) {
     
   std::list<int> num_list;
-  for(unsigned int i=1; i<argc; i++) {
+  for(unsigned int i=0; i< argc; i++) {
     num_list.push_back(std::atoi(argv[i]));
   }
-
-  for(std::list<int>::iterator it = num_list.begin(); it != num_list.end(); ++it) {
-    std::cout << *it << " ";
+  std::list<int>::iterator it = num_list.end();
+  while (it != num_list.begin()) {
+        std::cout << *it*2 << " ";
+        it--;
   }
 
   return 0;
